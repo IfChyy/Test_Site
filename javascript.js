fa4199567f39dc5710faa0d257a7164af4f5fd94
ACER <dasda>
2016-04-22 21:50:37 +0100

    danoooo

--- javascript.js
+++ javascript.js
@@ -119,7 +119,30 @@ function initMap() {
         handleLocationError(false, infoWindow, map.getCenter());
     }
 
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+        });
 
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+        });
+
+
+    }
+
+    function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
+        }
+    }
+
+    
     var service = new google.maps.places.PlacesService(map);
     service.nearbySearch({
         location: pos,
@@ -131,28 +154,9 @@ function initMap() {
 
 
 
-function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-        }
-    }
-}
-
-function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-    });
 
 
-}
+
 
 
 
