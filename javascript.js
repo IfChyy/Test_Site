76e41e37d6fd08d6237f95a86c4927810660d5bf
ACER <dasda>
2016-04-22 21:34:58 +0100

    danoooo

--- javascript.js
+++ javascript.js
@@ -91,18 +91,18 @@ function createMarker(place) {
 
 
 
-
-
-
-
-
-
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 13
     });
-    var infoWindow = new google.maps.InfoWindow({map: map});
+    var infoWindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: map,
+        radius: 800,
+        type: ['bar']
+    }, callback);
 
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
@@ -122,7 +122,44 @@ function initMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
+
+
+
 }
+
+
+
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+
+
+}
+
+
+
+
+
+
+
+
+
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
