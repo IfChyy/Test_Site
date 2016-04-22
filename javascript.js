1ba7ca4569f950c1122aa9e46254153deb9f5569
Simeon Simeonov <simeonov21@gmail.com>
2016-04-22 18:08:20 +0100

    yjgfjf

--- javascript.js
+++ javascript.js
@@ -44,70 +44,7 @@ function initMap() {
         handleLocationError(false, infoWindow, map.getCenter());
     }
 
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-        location: {lat: -34.397, lng: 150.644},
-        radius: 500,
-        type: ['bar']
-    }, processResults);
-
 }
-
-
-
-function processResults(results, status, pagination) {
-    if (status !== google.maps.places.PlacesServiceStatus.OK) {
-        return;
-    } else {
-        createMarkers(results);
-
-        if (pagination.hasNextPage) {
-            var moreButton = document.getElementById('more');
-
-            moreButton.disabled = false;
-
-            moreButton.addEventListener('click', function() {
-                moreButton.disabled = true;
-                pagination.nextPage();
-            });
-        }
-    }
-}
-
-function createMarkers(places) {
-    var bounds = new google.maps.LatLngBounds();
-    var placesList = document.getElementById('places');
-
-    for (var i = 0, place; place = places[i]; i++) {
-        var image = {
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(25, 25)
-        };
-
-        var marker = new google.maps.Marker({
-            map: map,
-            icon: image,
-            title: place.name,
-            position: place.geometry.location
-        });
-
-        placesList.innerHTML += '<li>' + place.name + '</li>';
-
-        bounds.extend(place.geometry.location);
-    }
-    map.fitBounds(bounds);
-}
-
-
-
-
-
-
-
-
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
