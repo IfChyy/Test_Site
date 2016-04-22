682cb55e5e106b95f41722c8f67e2759f50f659e
azifchyy <azifchyy>
2016-04-22 21:27:45 +0300

    EBAGIO5

--- javascript.js
+++ javascript.js
@@ -19,6 +19,8 @@ window.fbAsyncInit = function() {
 }(document, 'script', 'facebook-jssdk'));
 
 function initMap() {
+
+    var pyrmont = {lat: -34.397, lng: 150.644};
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 12
