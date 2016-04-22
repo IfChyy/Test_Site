d2e405417ece96c09a45ef4b97c82fd6a44baa47
Simeon Simeonov <simeonov21@gmail.com>
2016-04-22 17:49:08 +0100

    yjgfjf

--- javascript.js
+++ javascript.js
@@ -43,6 +43,26 @@ function initMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
+    var infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+
+    service.getDetails({
+        placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'
+    }, function(place, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            var marker = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location
+            });
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+                    'Place ID: ' + place.place_id + '<br>' +
+                    place.formatted_address + '</div>');
+                infowindow.open(map, this);
+            });
+        }
+    });
+
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
