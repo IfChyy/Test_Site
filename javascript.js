c01a871a8a2eeb26ee412d60c58395bd9303da12
azifchyy <azifchyy>
2016-04-22 22:00:24 +0300

    8

--- javascript.js
+++ javascript.js
@@ -18,7 +18,7 @@ window.fbAsyncInit = function() {
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-
+/**
 var map;
 var infowindow;
 
@@ -28,10 +28,45 @@ function initMap() {
         zoom: 12
     });
     var infoWindow = new google.maps.InfoWindow({map: map});
-    
 
+    var pyrmont = {lat: -33.867, lng: 151.195};
 
-    // Try HTML5 geolocation.
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pyrmont,
+        zoom: 15
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: pyrmont,
+        radius: 500,
+        type: ['store']
+    }, callback);
+}
+
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
+
+   /* // Try HTML5 geolocation.
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             var pos = {
@@ -51,7 +86,7 @@ function initMap() {
     }
 
 
-}
+}*/
 
 var map;
 var infowindow;
