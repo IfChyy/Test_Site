ad803b55ba7d775c2817643d3c19d7fde5b00a97
ACER <dasda>
2016-04-22 22:15:09 +0100

    na manqka koda :D

--- javascript.js
+++ javascript.js
@@ -89,69 +89,81 @@ function createMarker(place) {
 }*/
 
 
+var map;
+var service;
+var marker;
+var pos;
+var infowindow;
+
+function initialize()
+{
 
 
-function initMap() {
-
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
-        zoom: 14
+        zoom: 13
     });
 
-    var infoWindow = new google.maps.InfoWindow();
-    var pos;
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
+    //HTML5 geolocation
+    if (navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition(function(position)
+            {
+                pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);
-        }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
+                infowindow = new google.maps.InfoWindow({map: map});
+
+                var request = {location:pos,radius:500,types: ['restaurant', 'cafe', 'bars']};
+
+                map.setCenter(pos);
+
+                
+                var service = new google.maps.places.PlacesService(map);
+                service.nearbySearch(request,callback);
+
+            },
+
+            function()
+            {
+                handleNoGeolocation(true);
+            });
+    }
+    else
+    {
+        handleNoGeolocation(false);
     }
 
-    function createMarker(place) {
+    function callback(results, status)
+    {
+        if (status == google.maps.places.PlacesServiceStatus.OK)
+        {
+            for (var i = 0; i < results.length; i++)
+            {
+                createMarker(results[i]);
+            }
+        }
+    }
+
+    function createMarker(place)
+    {
         var placeLoc = place.geometry.location;
         var marker = new google.maps.Marker({
             map: map,
             position: place.geometry.location
         });
 
-        google.maps.event.addListener(marker, 'click', function() {
+        google.maps.event.addListener(marker, 'click', function()
+        {
             infowindow.setContent(place.name);
             infowindow.open(map, this);
         });
 
-
     }
-
-    function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                createMarker(results[i]);
-            }
-        }
-    }
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-        location: pos,
-        radius: 2000,
-        type: ['bar']
-    }, callback);
-
 }
-
-
-
+google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
