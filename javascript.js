105e3e66d834d124ce88836028eb471929f201ba
azifchyy <azifchyy>
2016-04-22 20:32:19 +0300

    EBAGIO2

--- javascript.js
+++ javascript.js
@@ -49,14 +49,12 @@ function initMap() {
         handleLocationError(false, infoWindow, map.getCenter());
     }
 
-    var request = {
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
         location: pyrmont,
-        radius: '22500',
-        query: ['bar']
-        
-    };
-    service = new google.maps.places.PlacesService(map);
-    service.textSearch(request, callback);
+        radius: 500,
+        type: ['store']
+    }, processResults);
 }
 
 function callback(results, status) {
@@ -68,6 +66,51 @@ function callback(results, status) {
     }
 }
 
+function processResults(results, status, pagination) {
+    if (status !== google.maps.places.PlacesServiceStatus.OK) {
+        return;
+    } else {
+        createMarkers(results);
+
+        if (pagination.hasNextPage) {
+            var moreButton = document.getElementById('more');
+
+            moreButton.disabled = false;
+
+            moreButton.addEventListener('click', function() {
+                moreButton.disabled = true;
+                pagination.nextPage();
+            });
+        }
+    }
+}
+
+function createMarkers(places) {
+    var bounds = new google.maps.LatLngBounds();
+    var placesList = document.getElementById('places');
+
+    for (var i = 0, place; place = places[i]; i++) {
+        var image = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+        };
+
+        var marker = new google.maps.Marker({
+            map: map,
+            icon: image,
+            title: place.name,
+            position: place.geometry.location
+        });
+
+        placesList.innerHTML += '<li>' + place.name + '</li>';
+
+        bounds.extend(place.geometry.location);
+    }
+    map.fitBounds(bounds);
+}
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
