f54901c0e53c487e517bcaacb20647ddac531c76
ACER <dasda>
2016-05-02 17:43:23 +0100

    probaa

--- javascript.js
+++ javascript.js
@@ -101,9 +101,9 @@ function initMap()
     var directionsService = new google.maps.DirectionsService();
     var directionsDisplay = new google.maps.DirectionsRenderer();
     //57.119234, -2.138756
-    //var myLatLng = new google.maps.LatLng({lat:  pos.lat, lng: pos.lng});
+    var myLatLng = new google.maps.LatLng({lat:  pos.lat, lng: pos.lng});
 
-    var myLatLng = new google.maps.LatLng({lat:  57.119234, lng: -2.138756});
+    //var myLatLng = new google.maps.LatLng({lat:  57.119234, lng: -2.138756});
     directionsDisplay.setMap(map);
     var request = {
         origin:  myLatLng,
